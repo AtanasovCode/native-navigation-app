eb803d8816e60a805570dcaeeaa3b017f9183325
AtanasovCode <a_ljupce@yahoo.com>
2024-05-03 19:57:29 +0200

    fixed responsiveness issue with homescreen - moved the image from <ImageBackground> to an <Image /> tag - removed FlatList due to scrolling issue - changed from fixed height for image background

--- routes/HomeScreen.jsx
+++ routes/HomeScreen.jsx
@@ -3,52 +3,31 @@ import { View, Text, StatusBar, FlatList, Image, ImageBackground, TouchableHighl
 import { StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { data } from '../components/Data';
+import { Dimensions } from 'react-native';
 const background = { uri: 'https://images.unsplash.com/photo-1620984721051-36dafb139faa?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' };
 
 import { MaterialIcons } from '@expo/vector-icons';
 
 const HomeScreen = ({ navigation }) => {
 
-    const renderItem = ({ item }) => {
-        return (
-            <TouchableHighlight
-                style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-                onPress={() => navigation.navigate("Explanation", {
-                    name: item.name,
-                    id: item.id,
-                    title: item.title,
-                    description: item.description,
-                    uri: item.uri,
-                    type: item.type,
-                })}
-            >
-                <View style={renderStyles.container}>
-                    <View style={renderStyles.iconWrapper}>
-                        {item.icon}
-                    </View>
-                    <Text style={[renderStyles.baseText, renderStyles.title]}>
-                        {item.name}
-                    </Text>
-                </View>
-            </TouchableHighlight>
-        );
-    };
+    const screenHeight = Dimensions.get('window').height; // Get screen width
 
     return (
-        <View style={styles.container}>
-            <ImageBackground
-                source={background}
-                style={styles.imageBackground}
-            >
+        <View style={[styles.container, { minHeight: screenHeight }]}>
+            <View style={styles.imageContainer}>
+                <Image
+                    source={background}
+                    style={styles.image}
+                />
                 <LinearGradient
                     colors={['rgba(0, 0, 0, 0)', '#1a1a1b']}
                     style={styles.gradient}
                     locations={[0.655, 1]}
                 />
-            </ImageBackground>
+            </View>
             <View style={styles.wrapper}>
-                <Text style={[styles.title, {marginBottom: 8}]}>Take the Quiz</Text>
-                <Text style={[renderStyles.baseText, {marginBottom: 16}]}>
+                <Text style={[styles.title, { marginBottom: 8 }]}>Take the Quiz</Text>
+                <Text style={[renderStyles.baseText, { marginBottom: 16 }]}>
                     Find out which E-bike is best for you
                 </Text>
                 <TouchableHighlight
@@ -74,11 +53,33 @@ const HomeScreen = ({ navigation }) => {
                     Learn About
                 </Text>
             </View>
-            <FlatList
-                data={data}
-                renderItem={renderItem}
-                keyExtractor={(item) => item.id.toString()}
-            />
+            {
+                data.map((item) => {
+                    return (
+                        <TouchableHighlight
+                            key={item.id}
+                            style={{ alignItems: "center", justifyContent: "center" }}
+                            onPress={() => navigation.navigate("Explanation", {
+                                name: item.name,
+                                id: item.id,
+                                title: item.title,
+                                description: item.description,
+                                uri: item.uri,
+                                type: item.type,
+                            })}
+                        >
+                            <View style={renderStyles.container}>
+                                <View style={renderStyles.iconWrapper}>
+                                    {item.icon}
+                                </View>
+                                <Text style={[renderStyles.baseText, renderStyles.title]}>
+                                    {item.name}
+                                </Text>
+                            </View>
+                        </TouchableHighlight>
+                    );
+                })
+            }
         </View>
     );
 }
@@ -105,7 +106,7 @@ const renderStyles = StyleSheet.create({
         textAlign: "center",
     },
     title: {
-        fontSize: 24,
+        fontSize: 20,
         fontWeight: "bold",
         textAlign: "left",
         width: "100%",
@@ -124,12 +125,15 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: "#1a1a1b",
         flex: 1,
+        justifyContent: "flex-start"
     },
-    imageBackground: {
-        height: 240,
-        width: "100%",
-        resizeMode: "cover",
+    imageContainer: {
+        height: "20%",
         marginBottom: 22,
+        position: "relative",
+    },
+    image: {
+        flex: 1
     },
     gradient: {
         position: "absolute",
